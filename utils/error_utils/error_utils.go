3d76d206ba5d8d8206502763929f53e12cf34da8
steven victor <chikodi543@gmail.com>
2019-11-27 00:41:15 +0100

    Implement error interface

---
+++ utils/error_utils/error_utils.go
@@ -0,0 +1,54 @@
+package error_utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type MessageErr interface {
+	Message() string
+	Status() int
+	Error() string
+}
+
+type messageErr struct {
+	ErrMessage string  `json:"message"`
+	ErrStatus  int           `json:"status"`
+	ErrError   string        `json:"error"`
+}
+
+func (e *messageErr) Error() string {
+	return e.ErrError
+}
+
+func (e *messageErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e *messageErr) Status() int {
+	return e.ErrStatus
+}
+
+func NewNotFoundError(message string) MessageErr {
+	return &messageErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusNotFound,
+		ErrError:   "not_found",
+	}
+}
+
+func NewBadRequestError(message string) MessageErr {
+	return &messageErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusBadRequest,
+		ErrError:   "invalid_request",
+	}
+}
+
+func NewApiErrFromBytes(body []byte) (MessageErr, error) {
+	var result messageErr
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
