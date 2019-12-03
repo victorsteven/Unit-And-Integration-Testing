2684c5440f3087920e9077c0b6f74488173e8330
steven victor <chikodi543@gmail.com>
2019-12-03 11:49:16 +0100

    Improve coverage in test cases

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -201,8 +201,19 @@ func TestCreateMessage(t *testing.T) {
 			statusCode: 422,
 			errMessage: "Please enter a valid body",
 		},
+		{
+			//when an integer is used like a string for title
+			inputJSON:  `{"title": 12345, "body": "the body"}`,
+			statusCode: 422,
+			errMessage: "invalid json body",
+		},
+		{
+			//when an integer is used like a string for body
+			inputJSON:  `{"title": "the title", "body": 123453 }`,
+			statusCode: 422,
+			errMessage: "invalid json body",
+		},
 	}
-
 	for _, v := range samples {
 		r := gin.Default()
 		r.POST("/messages", controllers.CreateMessage)
@@ -245,6 +256,7 @@ func TestGetMessageByID(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error while seeding table: %s", err)
 	}
+
 	samples := []struct {
 		id         string
 		statusCode int
@@ -301,6 +313,9 @@ func TestGetMessageByID(t *testing.T) {
 
 func TestUpdateMessage(t *testing.T) {
 
+	//var firstTitle, firstBody string
+	var firstId int64
+
 	Database()
 
 	gin.SetMode(gin.TestMode)
@@ -309,11 +324,21 @@ func TestUpdateMessage(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	message, err := seedOneMessage()
-	fmt.Println("this is the error: ", message)
+	messages, err := seedMessages()
 	if err != nil {
 		t.Errorf("Error while seeding table: %s", err)
 	}
+
+	//Get only the first message
+	for _, message := range messages {
+		if message.Id == 2 {
+			continue
+		}
+		//firstTitle = message.Title
+		//firstBody = message.Body
+		firstId = message.Id
+	}
+
 	samples := []struct {
 		id          string
 		inputJSON  string
@@ -323,7 +348,7 @@ func TestUpdateMessage(t *testing.T) {
 		errMessage string
 	}{
 		{
-			id:          strconv.Itoa(int(message.Id)),
+			id:          strconv.Itoa(int(firstId)),
 			inputJSON:  `{"title":"update title", "body": "update body"}`,
 			statusCode: 200,
 			title:      "update title",
@@ -331,18 +356,39 @@ func TestUpdateMessage(t *testing.T) {
 			errMessage: "",
 		},
 		{
+			// "second title" belongs to the second message so, the cannot be used for the first message
+			id:         strconv.Itoa(int(firstId)),
+			inputJSON:  `{"title":"second title", "body": "update body"}`,
+			statusCode: 500,
+			errMessage: "title already taken",
+		},
+		{
 			//Empty title
-			id:          strconv.Itoa(int(message.Id)),
+			id:          strconv.Itoa(int(firstId)),
 			inputJSON:  `{"title":"", "body": "update body"}`,
 			statusCode: 422,
 			errMessage: "Please enter a valid title",
 		},
 		{
-			//Empty title
-			id:          strconv.Itoa(int(message.Id)),
+			//Empty body
+			id:          strconv.Itoa(int(firstId)),
 			inputJSON:  `{"title":"the title", "body": ""}`,
 			statusCode: 422,
 			errMessage: "Please enter a valid body",
+		},
+		{
+			//when an integer is used like a string for title
+			id:          strconv.Itoa(int(firstId)),
+			inputJSON:  `{"title": 12345, "body": "the body"}`,
+			statusCode: 422,
+			errMessage: "invalid json body",
+		},
+		{
+			//when an integer is used like a string for body
+			id:          strconv.Itoa(int(firstId)),
+			inputJSON:  `{"title": "the title", "body": 123453 }`,
+			statusCode: 422,
+			errMessage: "invalid json body",
 		},
 		{
 			id:         "unknwon",
@@ -383,7 +429,6 @@ func TestUpdateMessage(t *testing.T) {
 	}
 }
 
-
 func TestGetAllMessage(t *testing.T) {
 
 	Database()
@@ -416,5 +461,69 @@ func TestGetAllMessage(t *testing.T) {
 	}
 	assert.Equal(t, rr.Code, http.StatusOK)
 	assert.Equal(t, len(msgs), 2)
+}
+
+func TestDeleteMessage(t *testing.T) {
+
+	Database()
+
+	gin.SetMode(gin.TestMode)
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	message, err := seedOneMessage()
+	if err != nil {
+		t.Errorf("Error while seeding table: %s", err)
+	}
+	samples := []struct {
+		id         string
+		statusCode int
+		status string
+		errMessage string
+	}{
+		{
+			id:         strconv.Itoa(int(message.Id)),
+			statusCode: 200,
+			status: "deleted",
+			errMessage: "",
+		},
+		{
+			id:         "unknwon",
+			statusCode: 400,
+			errMessage: "message id should be a number",
+		},
+		{
+			id:         strconv.Itoa(12322), //an id that does not exist
+			statusCode: 404,
+			errMessage: "no record matching given id",
+		},
+	}
+	for _, v := range samples {
+		r := gin.Default()
+		r.DELETE("/messages/:message_id", controllers.DeleteMessage)
+		req, err := http.NewRequest(http.MethodDelete, "/messages/"+v.id, nil)
+		if err != nil {
+			t.Errorf("this is the error: %v\n", err)
+		}
+		rr := httptest.NewRecorder()
+		r.ServeHTTP(rr, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal(rr.Body.Bytes(), &responseMap)
+		if err != nil {
+			t.Errorf("Cannot convert to json: %v", err)
+		}
+		assert.Equal(t, rr.Code, v.statusCode)
+
+		if v.statusCode == 200 {
+			//casting the interface to map:
+			assert.Equal(t, responseMap["status"], v.status)
+		}
+		if v.statusCode == 400 || v.statusCode == 422 && v.errMessage != "" {
+			assert.Equal(t, responseMap["message"], v.errMessage)
+		}
+	}
 }
 
